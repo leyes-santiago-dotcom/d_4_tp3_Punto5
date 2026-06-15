9c1dba923c5575bc8fb403be2abd088904fb11e5
leyes-santiago-dotcom <leyessantifacultad@gmail.com>
2026-06-15 07:23:58 -0300

    Diseño de interfaz

--- d_4_tp3_Punto5/Form1.Designer.vb
+++ d_4_tp3_Punto5/Form1.Designer.vb
@@ -22,16 +22,51 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        cmdEjecutar = New Button()
+        lstVector10 = New ListBox()
+        lstVector15 = New ListBox()
         SuspendLayout()
+        ' 
+        ' cmdEjecutar
+        ' 
+        cmdEjecutar.Location = New Point(12, 12)
+        cmdEjecutar.Name = "cmdEjecutar"
+        cmdEjecutar.Size = New Size(94, 29)
+        cmdEjecutar.TabIndex = 0
+        cmdEjecutar.Text = "Ejecutar"
+        cmdEjecutar.UseVisualStyleBackColor = True
+        ' 
+        ' lstVector10
+        ' 
+        lstVector10.FormattingEnabled = True
+        lstVector10.Location = New Point(12, 47)
+        lstVector10.Name = "lstVector10"
+        lstVector10.Size = New Size(150, 244)
+        lstVector10.TabIndex = 1
+        ' 
+        ' lstVector15
+        ' 
+        lstVector15.FormattingEnabled = True
+        lstVector15.Location = New Point(168, 47)
+        lstVector15.Name = "lstVector15"
+        lstVector15.Size = New Size(150, 384)
+        lstVector15.TabIndex = 2
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(330, 450)
+        Controls.Add(lstVector15)
+        Controls.Add(lstVector10)
+        Controls.Add(cmdEjecutar)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents cmdEjecutar As Button
+    Friend WithEvents lstVector10 As ListBox
+    Friend WithEvents lstVector15 As ListBox
 
 End Class
