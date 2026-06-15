9e144996abeef98ad67e060f67a1af81999cd6e1
leyes-santiago-dotcom <leyessantifacultad@gmail.com>
2026-06-15 07:21:46 -0300

    Add project files.

---
+++ d_4_tp3_Punto5/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    '4. Realizar un algoritmo que permita cargar un vector de 10 elementos enteros. A partir del 
+    'vector original generar un nuevo vector de 15 elementos, tal que el primer elemento del 
+    'vector original pasa al primer elemento del segundo vector y el segundo elemento del 
+    'original en el segundo elemento del vector nuevo y el tercer elemento del nuevo vector 
+    'se forma con la suma de los 2 anteriores. Con los nuevos elementos se repite la secuencia. 
+    'Mostrar en Pantalla los 2 vectores. 
+
+
+
+
+
+
+
+End Class
